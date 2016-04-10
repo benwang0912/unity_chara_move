086154bccc5777894f0a1cd07c917e0299cecda0
F74024054 <benwang0912@gmail.com>
2016-04-10 23:21:52 +0800

    modified:   .vs/character moving/v14/.suo 	modified:   Assets/Animator_trig.cs 	modified:   Assets/Raw Mocap Data/Animations/Running/Jumping/JogJump_ToLeft_2.fbx.meta 	modified:   Assets/sceen1.unity 	modified:   Library/CurrentLayout.dwlt 	modified:   Library/InspectorExpandedItems.asset 	modified:   Library/ScriptAssemblies/Assembly-CSharp.dll 	modified:   Library/ScriptAssemblies/Assembly-CSharp.dll.mdb 	modified:   Library/assetDatabase3 	modified:   Library/metadata/00/00000000000000003000000000000000 	modified:   Library/metadata/00/00000000000000004000000000000000 	modified:   Library/metadata/00/00000000000000006100000000000000 	modified:   Library/metadata/a2/a2f67b507c47f0341b039ff4c4479b23 	modified:   Library/metadata/e8/e86121e4f110f884bb4b59496e3adbef 	modified:   ProjectSettings/TagManager.asset

--- Assets/Animator_trig.cs
+++ Assets/Animator_trig.cs
@@ -3,56 +3,74 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class Animator_trig : MonoBehaviour {
     private Animator animator;
-    private CharacterController controller;
     public Rigidbody rb;
     public float speed =10.0f;
     public float turn_speed = 60.0f;
     public Vector3 moveDirection = Vector3.zero;
-    public float gravity = 20.0f;
-    public float jumpHeight=5000f;
+    public float jumpHeight=40f;
+    //---------------------------------------ground checking
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    Collider[] groundCollisions;
+    float groundCheckRadius = 0.2f;
+    bool grounded = false;
+
     // Use this for initialization
     void Start () {
         animator = gameObject.GetComponentInChildren<Animator>();
-        controller = gameObject.GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+   /* bool IsGrounded(){
+        RaycastHit hit;
+        //Debug.DrawRay(transform.position, transform.up * 10f, Color.yellow);
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit,0.5f))
+        {
+            Debug.Log(Vector3.Distance(transform.position, hit.collider.transform.position));
+            if (Vector3.Distance(transform.position, hit.collider.transform.position) <= 0.2f)
+                return true;
+        }
+        
+        return false;
+    }*/
+
+// Update is called once per frame
+    void Update () {
         if (Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.W))
         {
-            animator.SetInteger("Anim",1);
+            if(grounded)
+                animator.SetInteger("Anim",1);
+            transform.position += transform.forward *  speed * Time.deltaTime;
         }
         else
         {
             animator.SetInteger("Anim", 0);
         }
 
-        if (Input.GetAxis("Jump")>0 && controller.isGrounded)
+        if (Input.GetAxis("Jump")>0 && grounded)
         {
             animator.SetInteger("Jump", 1);
             rb.AddForce(transform.up * jumpHeight);
+            grounded = false;
         }
         else
         {
             animator.SetInteger("Jump", 0);
         }
-
-        if (controller.isGrounded)
-        {
-            if(Input.GetAxis("Vertical")>0)
-                moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-            else
-                moveDirection = Vector3.zero;
-        }
+       // IsGrounded();
         float turn = Input.GetAxis("Horizontal");
         transform.Rotate(0,turn*turn_speed*Time.deltaTime,0);
-        controller.Move(moveDirection*Time.deltaTime);
-        moveDirection.y -= gravity * Time.deltaTime;
-
         if (transform.position.y<-10)
         {
             transform.position = Vector3.zero;
         }
 	}
+    void FixedUpdate()
+    {
+        groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if (groundCollisions.Length > 0)
+            grounded = true;
+        else
+            grounded = false;
+    }
 }
